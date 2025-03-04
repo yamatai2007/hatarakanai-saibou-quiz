81c10cc6e4a5563d5fce2e2122ca9c383973cc28
yamatai2007 <taiga_y10092258@icloud.com>
2025-03-04 16:47:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.getElementById("nextButton");
+
     if (nextButton) {
         nextButton.addEventListener("click", function () {
             const groupName = document.getElementById("groupName").value;
